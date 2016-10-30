59743db55ed395447f1e253cfa0a983ee8b30117
Dmitry Zhuk <dima_zhuk98@mail.ru>
2016-10-30 14:02:14 +0300

    add helper, styles, the output temperature and pressure and html search

--- app/js/script.js
+++ app/js/script.js
@@ -5,8 +5,17 @@ $(document).ready(function () {
     var app_ID = '4bcc1ce632994fe5647884e0fae87f01';
     var minsk_ID = 625144;
 
+    $("#country-search-action").on('submit', function(event){
+        var country = $("#country-user").val();
+        console.log(country);
+    });
+
     var renderingWeather = function() {
         var template = Handlebars.compile(source);
+        Handlebars.registerHelper('pressureMm', function() {
+            return Math.round(this.main.pressure * 0.75006375541921);
+        });
+
         var getJSON = function(url) {
             return new Promise(function(resolve, reject) {
                 var xhr = new XMLHttpRequest();
@@ -26,16 +35,14 @@ $(document).ready(function () {
 
         };
         //http://api.openweathermap.org/data/2.5/forecast?id='+ minsk_ID +'&APPID=' + app_ID
-        getJSON('http://api.openweathermap.org/data/2.5/forecast?q=Minsk,by' +'&APPID=' + app_ID).then(function(data) {
+        getJSON('http://api.openweathermap.org/data/2.5/forecast?q=Minsk' +'&units=metric&APPID=' + app_ID).then(function(data) {
             var weatherTemplate = document.getElementById("weather-template").innerHTML;
             var compileTemplate = Handlebars.compile(weatherTemplate);
             var result = compileTemplate(data);
             var content = document.getElementById("content");
             content.innerHTML = result;
-            //console.log(result);
+            
             console.log(data);
-            console.log(data.list[0].dt_txt);
-            console.log(data.list[0].weather[0].description);
         }, function(status) {
             alert('Something went wrong.');
         });
