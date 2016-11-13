78e1d4497fe65d028fc791cd92b95abb9aa44615
Dmitry Zhuk <dima_zhuk98@mail.ru>
2016-11-13 23:33:26 +0300

    add promise

--- app/js/script.js
+++ app/js/script.js
@@ -33,86 +33,41 @@ $(document).ready(function () {
         Handlebars.registerHelper('pressureMm', function() {
             return Math.round(this.main.pressure * 0.75006375541921);
         });
-        var xhr = new XMLHttpRequest();
-        xhr.open('get', 'http://api.openweathermap.org/data/2.5/forecast?q=' + city +'&units=metric&APPID=' + app_ID, true);
-        // xhr.responseType = 'json';
-        xhr.onload = function(){
-            var status = xhr.status;
-            if (status == 200){
-                var data = JSON.parse(xhr.responseText);
 
-                var source = $("#weather-template").html();
-                var template = Handlebars.compile(source);
-                var result = template(data);
-                var content = $("#content")[0];
-                content.innerHTML = result;
-                console.log(data);
-            }else{
-                console.log("Error request: " + xhr.status + " " + xhr.statusText);
-                $('.alert').remove();
-                var tmpl = '<div class = "alert alert-danger alert-dismissible" role="alert">' +
-                '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
-                '<span aria-hidden="true">&times;</span></button>' +
-                '<strong>Error!</strong> '+ xhr.status + " " + xhr.statusText + '.' + '</div>';
-                $('#alert').append(tmpl);
-            }
-        }
-        xhr.onerror = function(){
-            console.log(status);
-        }
-        xhr.send();
+
+        // xhr.responseType = 'json';
+        return new Promise(function(resolve, reject){
+            var xhr = new XMLHttpRequest();
+            xhr.open('get', 'http://api.openweathermap.org/data/2.5/forecast?q=' + city +'&units=metric&APPID=' + app_ID, true);
+            xhr.onload = function(){
+                var status = xhr.status;
+                if (status == 200){
+                    $('.alert').remove();
+                    var data = JSON.parse(xhr.responseText);
+
+                    var source = $("#weather-template").html();
+                    var template = Handlebars.compile(source);
+                    var result = template(data);
+                    var content = $("#content")[0];
+                    content.innerHTML = result;
+                    console.log(data);
+                    resolve(xhr.response);
+                }else{
+                    console.log("Error request: " + xhr.status + " " + xhr.statusText);
+                    $('.alert').remove();
+                    var tmpl = '<div class = "alert alert-danger alert-dismissible" role="alert">' +
+                    '<button type="button" class="close" data-dismiss="alert" aria-label="Close">' +
+                    '<span aria-hidden="true">&times;</span></button>' +
+                    '<strong>Error!</strong> '+ xhr.status + " " + xhr.statusText + '.' + '</div>';
+                    $('#alert').append(tmpl);
+                    reject(status);
+                }
+            };
+            xhr.onerror = function(){
+                //console.log("Ошибка!");
+                reject(status);
+            };
+            xhr.send();
+        });
     }
 });
-
-    // var renderingWeather = function(city) {
-    //     console.log(city);
-    //     // var template = Handlebars.compile(source);
-    //     Handlebars.registerHelper('pressureMm', function() {
-    //         return Math.round(this.main.pressure * 0.75006375541921);
-    //     });
-
-    //     var getJSON = function(url) {
-    //         return new Promise(function(resolve, reject) {
-    //             var xhr = new XMLHttpRequest();
-    //             xhr.open('get', url, true);
-    //             xhr.responseType = 'json';
-    //             xhr.onload = function() {
-    //                 var status = xhr.status;
-    //                 if (status == 200) {
-
-    //                     var source = $("#weather-template").html();
-
-    //                     //var json = JSON.parce()
-    //                     resolve(xhr.response);
-    //                 } else {
-    //                     reject(status);
-    //                 }
-    //             };
-    //             xhr.send();
-    //         });
-
-    //     };
-    //     //http://api.openweathermap.org/data/2.5/forecast?id='+ minsk_ID +'&APPID=' + app_ID
-    //     getJSON('http://api.openweathermap.org/data/2.5/forecast?q=' + city +'&units=metric&APPID=' + app_ID).then(function(data) {
-
-    //         var template = Handlebars.compile(source);
-    //         console.log(data);
-    //         var weatherTemplate = document.getElementById("weather-template").innerHTML;
-    //         var compileTemplate = Handlebars.compile(weatherTemplate);
-    //         var result = template(data);
-    //         var content = document.getElementById("content");
-    //         content.innerHTML = result;
-
-    //         console.log(data);
-    //     }, function(status) {
-    //         alert('Something went wrong.');
-    //     });
-
-    // }
-
-
-
-
-
-
-    // setInterval(renderData, 1000)
