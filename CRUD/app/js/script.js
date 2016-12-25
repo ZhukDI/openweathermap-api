8413e7e6be0cc79656fbef2ab29c05937495800b
Dmitry Zhuk <dima_zhuk98@mail.ru>
2016-12-25 18:19:22 +0300

    add class Team

--- CRUD/app/js/script.js
+++ CRUD/app/js/script.js
@@ -34,13 +34,26 @@ class Programmer extends Person{
 	}
 }
 
+class Team{
+	constructor(name, task, tmLeader, members, id) {
+    	this.name = name;
+		this.task = task;
+		this.tmLeader = tmLeader;
+		this.members = members;
+		this.id = id;
+		Team.id += 1;
+	}
+}
+
 Person.id = 0; //статическая переменная
+Team.id = 0;
 
 
-
-//let pr = new Programmer("Дима", 18, 500, "android-developer", Person.id);
-//let pr2 = new Programmer("Рома", 18, 400, "android-developer", Person.id);
-//let tl = new TeamLeader("Олег", 25, 700, "oleg@mail.ru", Person.id)
+let pr = new Programmer("Дима", 18, 500, "android-developer", Person.id);
+let pr2 = new Programmer("Рома", 18, 400, "android-developer", Person.id);
+let tl = new TeamLeader("Олег", 25, 700, "oleg@mail.ru", Person.id);
+let tm = new Team("Dream", "task1", tl,[pr, pr2], Team.id);
+console.log(tm);
 //pr.sayHi();
 //pr2.sayHi();
 //console.log(Person.id);
@@ -50,6 +63,7 @@ Person.id = 0; //статическая переменная
 
 let programmers = [];
 let teamLeaders = [];
+let teams = [];
 
 let compileProgrammer = () => {
 	let data = programmers;
@@ -104,15 +118,18 @@ let updateProgrammer = (id) => {
 
 $(document).on("click", ".addProgrammer", function(){
     addProgrammer();
+	compileProgrammerName();
 });
 
 $(document).on("click", ".deleteProgrammer", function(){
     deleteProgrammer(this.id);
+	compileProgrammerName();
 	console.log(this.id);
 });
 
 $(document).on("click", ".updateProgrammer", function(){
 	updateProgrammer(this.id);
+	compileProgrammerName();
 	deleteProgrammer(this.id);
 });
 
@@ -173,20 +190,90 @@ let updateTeamLeader = (id) => {
 
 $(document).on("click", ".addTeamLeader", function(){
     addTeamLeader();
+	compileTeamLeaderName();
 });
 
 $(document).on("click", ".deleteTeamLeader", function(){
     deleteTeamLeader(this.id);
+	compileTeamLeaderName();
 	console.log(this.id);
 });
 
 $(document).on("click", ".updateTeamLeader", function(){
 	updateTeamLeader(this.id);
+	compileTeamLeaderName();
 	deleteTeamLeader(this.id);
 });
 
 
 
+
+
+
+
+
+let compileTeamLeaderName = () => {
+	let data = teamLeaders;
+		
+	let source = $("#teamLeaderName-template").html();
+    let template = Handlebars.compile(source);
+    let result = template(data);
+    let content = $("#tlList")[0];
+    content.innerHTML = result;	
+}
+
+let compileProgrammerName = () => {
+	let data = programmers;
+		
+	let source = $("#programmerName-template").html();
+    let template = Handlebars.compile(source);
+    let result = template(data);
+    let content = $("#prList")[0];
+    content.innerHTML = result;	
+}
+
+let searchTeamLeader = (id) => {
+	for(let i = 0;  i < teamLeaders.length; i++) {
+        if(teamLeaders[i].id == id) {
+            return teamLeaders[i];
+        }
+	}
+}
+
+let addTeam = () => {
+	event.preventDefault();
+	let name = $("#txtTmName").val();
+	let task = $("#txtTmTask").val();
+	//console.log($("#tlList").val());
+	
+	let tmpTeamLeader = searchTeamLeader($("#tlList").val());
+	
+	let tmpMembers = [];
+	//addMembers()
+	for(let i = 0; i < programmers.length; i++){
+		//let j = 
+		if($("#ch"+programmers[i].id).is(':checked')){
+			//console.log("work!");
+			tmpMembers.push(programmers[i]);
+		}
+	}
+	let tmpTeam = new Team(name, task, tmpTeamLeader, tmpMembers, Team.id);
+	console.log(tmpTeam);
+	//console.log(tmpTeamLeader);	
+//	$('#add-team-click')[0].reset();
+//	
+//	tmpTeamLeader = new TeamLeader(name, age, salary, email, Person.id);
+//	teamLeaders.push(tmpTeamLeader);
+//	console.log(teamLeaders);
+//	
+//	compileTeamLeader();
+}
+
+
+$(document).on("click", ".addTeam", function(){
+    addTeam();
+	//compileTeamLeaderName();
+});
 
 
 
